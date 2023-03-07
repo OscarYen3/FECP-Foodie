c2e8f1877b915c4d9dd21784e5940dfc43ef1568
OscarYen <mapd24th3ee@gmail.com>
2023-03-07 12:22:46 +0800

    new profile

---
+++ FECP-Foodie/ResultViewDlg.swift
@@ -0,0 +1,29 @@
+//
+//  ResultViewDlg.swift
+//  FECP-Foodie
+//
+//  Created by OscarYen on 2023/3/7.
+//
+
+import UIKit
+
+class ResultViewDlg: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
